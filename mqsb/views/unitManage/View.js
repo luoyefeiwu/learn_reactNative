2b6c951da0922beac0ad46317dba1e82a3d1326b
huangchuanwang <568384493.com>
2021-12-02 23:58:46 +0800

    列表页

--- mqsb/views/unitManage/View.js
+++ mqsb/views/unitManage/View.js
@@ -4,9 +4,10 @@ import {Input} from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-import {View, StyleSheet, FlatList, Text} from 'react-native';
+import {View, StyleSheet, FlatList, Text, Dimensions} from 'react-native';
 import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
 
+// 责任单位列表
 export default class UnitManage extends Component {
   constructor(props) {
     super(props);
@@ -15,22 +16,85 @@ export default class UnitManage extends Component {
 
   render() {
     return (
-      <View>
-        <Input
-          placeholder="请输入用户名"
-          leftIcon={<Icon name="search" size={16} />}
-          inputStyle={{
-            fontSize: 16,
-          }}
-          inputContainerStyle={{
-            height: 28,
-          }}
-          containerStyle={{
-            marginTop: 20,
-            height: 28,
-          }}
-        />
-        <Text>22311111111</Text>
+      <View
+        style={{
+          backgroundColor: '#efefff4',
+          minHeight: Dimensions.get('window').height,
+        }}>
+        <View
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            marginTop: 10,
+            marginBottom: 10,
+          }}>
+          <Input
+            placeholder="搜索"
+            leftIcon={<Icon name="search" size={16} color="grey" />}
+            leftIconContainerStyle={{marginLeft: 5}}
+            inputStyle={{
+              fontSize: 15,
+            }}
+            inputContainerStyle={{
+              height: 30,
+              borderRadius: 5,
+              backgroundColor: '#fff',
+              borderBottomColor: '#fff',
+            }}
+            containerStyle={{
+              height: 30,
+              flex: 9,
+            }}
+          />
+          <Text style={{flex: 1, color: 'green', marginRight: 10}}>取消</Text>
+        </View>
+        <View
+          style={{
+            paddingTop: 10,
+            marginBottom: 10,
+            borderTopWidth: 0.5,
+            borderTopColor: 'grey',
+          }}>
+          <Text style={{marginLeft: 10}}>为您找到相关结果23条</Text>
+        </View>
+        <View>
+          <FlatList
+            data={this.state.list}
+            keyExtractor={(item, index) => item.id}
+            // refreshing={this.props.refreshing}
+            // onRefresh={this.handleListRefresh}
+            renderItem={({item}) => {
+              return (
+                <View
+                  style={{
+                    backgroundColor: '#fff',
+                    marginLeft: 10,
+                    marginBottom: 10,
+                    marginRight: 10,
+                    borderRadius: 5,
+                    paddingLeft: 10,
+                  }}>
+                  <View
+                    style={{
+                      borderBottomWidth: 0.5,
+                      borderBottomColor: 'grey',
+                      height: 40,
+                      flexDirection: 'row',
+                      alignItems: 'center',
+                    }}>
+                    <Icon name="building" size={16} color="#2AC9FF" />
+                    <Text style={{marginLeft: 8}}>门店名称: {item.title}</Text>
+                  </View>
+                  <Text>注册名称:{item.title}</Text>
+                  <Text>责任人:{item.title}</Text>
+                  <Text>地址:{item.title}</Text>
+                  <Text>负责人电话:{item.title}</Text>
+                </View>
+              );
+            }}
+          />
+        </View>
       </View>
     );
   }
@@ -38,44 +102,11 @@ export default class UnitManage extends Component {
   componentDidMount() {
     let arr = [];
     for (let i = 0; i < 30; i++) {
-      arr.push({id: i, title: '2020-12-' + i});
+      arr.push({id: i, title: '上地大厦' + i});
     }
     this.setState({
       list: arr,
     });
   }
 }
-const styles = StyleSheet.create({
-  title: {
-    display: 'flex',
-    height: 40,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 2,
-    marginLeft: 2,
-    marginRight: 2,
-    backgroundColor: '#fff',
-  },
-  titleText: {
-    textAlign: 'center',
-  },
-  titleWidth: {
-    flex: 1,
-  },
-  content: {
-    marginTop: 2,
-    backgroundColor: '#fff',
-    display: 'flex',
-    flexDirection: 'row',
-    // minHeight: 500,
-  },
-  left: {
-    flex: 1,
-    borderRightColor: '#E0E0E0',
-    borderRightWidth: 1,
-  },
-  right: {
-    flex: 2,
-  },
-});
+const styles = StyleSheet.create({});
